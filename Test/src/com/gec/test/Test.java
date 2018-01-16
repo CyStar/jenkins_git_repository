aeb58fdc2afbffebc48edfb6d1fbefc3b3e30697
STAR <chenyongxing@flyaudio.cn>
2018-01-16 10:44:49 +0800

    second updat Test

--- Test/src/com/gec/test/Test.java
+++ Test/src/com/gec/test/Test.java
@@ -3,8 +3,7 @@ package com.gec.test;
 // test
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println("hello world");
+		int a = 10/0;
 		System.out.println("hello world");
 	}
 }
