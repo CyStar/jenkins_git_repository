4165808e14173cb60d6655f530225eabad96cf06
STAR <chenyongxing@flyaudio.cn>
2018-01-17 09:40:27 +0800

    update7 Test

--- Test/src/com/gec/test/Test.java
+++ Test/src/com/gec/test/Test.java
@@ -10,14 +10,9 @@ public class Test {
 		System.out.println("hello world");
 		System.out.println("hello world");
 		System.out.println("hello world");
-		
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
-		System.out.println("5");
-		
-		
+		System.out.println("hello world");
+		System.out.println("hello world");	
+		System.out.println("hello world");
 		
 	}
 }
